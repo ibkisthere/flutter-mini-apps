8e3765993f953e5d6501e41ae882432cc6161a0a
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-09-12 17:56:45 +0100

    feat:added using_valuenotifier

---
+++ using_valuenotifier/lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// managing state in flutter using a singleton ValueNotifier
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+      routes: {'/new-contact': (context) => NewContactView()},
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: ContactBook(),
+        builder: (contact, value, child) {
+          final contacts = value as List<Contact>;
+          return ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                final contact = contacts[index];
+                return Dismissible(
+                  onDismissed: (direction) {
+                    ContactBook().remove(contact: contact);
+                  },
+                  key: ValueKey(contact.id),
+                  child: Material(
+                    color: Colors.white,
+                    elevation: 6.0,
+                    child: ListTile(
+                      title: Text(contact.name),
+                    ),
+                  ),
+                );
+              });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Contact {
+  final String id;
+  final String name;
+  Contact({required this.name}) : id = Uuid().v4();
+}
+
+class ContactBook extends ValueNotifier<List<Contact>> {
+  ContactBook._sharedInstance() : super([]);
+
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = [];
+
+  int get length => value.length;
+
+  void add({required Contact contact}) {
+    final contacts = value;
+    contacts.add(contact);
+    notifyListeners();
+  }
+
+  void remove({required Contact contact}) {
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      notifyListeners();
+    }
+  }
+
+  Contact? contact({required int atIndex}) =>
+      value.length >= atIndex ? value[atIndex] : null;
+}
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a new contact'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration:
+                InputDecoration(hintText: 'Enter a new contact name here'),
+          ),
+          TextButton(
+              onPressed: () {
+                final contact = Contact(name: _controller.text);
+                ContactBook().add(contact: contact);
+                Navigator.of(context).pop();
+              },
+              child: Text('Add contact'))
+        ],
+      ),
+    );
+  }
+}
